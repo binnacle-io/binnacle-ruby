b081bfed0a2edd194279ca10a75943f41de373bd
Brian Sam-Bodden <bsbodden@integrallis.com>
2015-04-15 20:49:32 -0700

    On connection, driver will retrieve available endpoints and randomly choose one for the duration of the session

--- lib/binnacle/connection.rb
+++ lib/binnacle/connection.rb
@@ -1,5 +1,6 @@
 require 'forwardable'
 require 'faraday'
+require 'uri'
 
 module Binnacle
   class Connection
@@ -15,13 +16,37 @@ module Binnacle
 
       raise Binnacle::ConfigurationError.new("Binnacle URL not provided, set BINNACLE_URL or provided in the constructor") unless self.url
 
-      @connection ||= Faraday.new(:url => self.url) do |faraday|
+      build_connection(self.url)
+      randomize_endpoint
+    end
+
+    def endpoints
+      response = @connection.get do |req|
+        req.url "/api/endpoints"
+        req.headers['Content-Type'] = 'application/json'
+      end
+
+      JSON.parse(response.body)
+    end
+
+    def randomize_endpoint
+      list = endpoints
+      if endpoints.size > 1
+        uri = URI.parse(url)
+        endpoint = endpoints.sample
+        build_connection("#{uri.scheme}://#{endpoint}:#{uri.port}")
+      end
+    end
+
+    def build_connection(url)
+      @connection ||= Faraday.new(:url => url) do |faraday|
         faraday.request :basic_auth, api_key, api_secret
         faraday.request  :url_encoded             # form-encode POST params
         #faraday.response :logger                  # log requests to STDOUT TODO set a client log file
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      end 
+      end
     end
+
   end
 
 end
