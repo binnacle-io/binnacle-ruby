54b8e88d8c3a7e252e5f29a1e8438e323b4c9640
Brian Sam-Bodden <bsbodden@integrallis.com>
2015-04-16 10:09:48 -0700

    Fix connection randomizing code

--- binnacle.gemspec
+++ binnacle.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency 'faraday', '~> 0.9.1'
   spec.add_runtime_dependency 'faye-websocket', '~> 0.9.2'
-  spec.add_runtime_dependency 'trollop', '~> 2.1.1'
+  spec.add_runtime_dependency 'trollop', '~> 2.1.2'
 
   spec.add_development_dependency 'bundler', '~> 1.7'
   spec.add_development_dependency 'rake', '~> 10.0'
--- lib/binnacle/connection.rb
+++ lib/binnacle/connection.rb
@@ -7,16 +7,20 @@ module Binnacle
     extend Forwardable
 
     attr_reader :connection
-    attr_accessor :url
+    attr_reader :active_url
+    attr_reader :contact_url
 
     def_delegators :@connection, :get, :post, :put, :delete, :head, :patch, :options
 
     def initialize(api_key, api_secret, url = nil)
-      self.url = url || ENV['BINNACLE_URL']
+      @contact_url = url || ENV['BINNACLE_URL']
+      @active_url = @contact_url
+      @api_key = api_key
+      @api_secret = api_secret
 
-      raise Binnacle::ConfigurationError.new("Binnacle URL not provided, set BINNACLE_URL or provided in the constructor") unless self.url
+      raise Binnacle::ConfigurationError.new("Binnacle URL not provided, set BINNACLE_URL or provided in the constructor") unless @contact_url
 
-      build_connection(self.url)
+      build_connection
       randomize_endpoint
     end
 
@@ -29,18 +33,19 @@ module Binnacle
       JSON.parse(response.body)
     end
 
-    def randomize_endpoint
+    def randomize_endpoint()
       list = endpoints
       if endpoints.size > 1
         uri = URI.parse(url)
         endpoint = endpoints.sample
-        build_connection("#{uri.scheme}://#{endpoint}:#{uri.port}")
+        @active_url = "#{uri.scheme}://#{endpoint}:#{uri.port}"
+        build_connection()
       end
     end
 
-    def build_connection(url)
-      @connection ||= Faraday.new(:url => url) do |faraday|
-        faraday.request :basic_auth, api_key, api_secret
+    def build_connection()
+      @connection ||= Faraday.new(:url => @active_url) do |faraday|
+        faraday.request :basic_auth, @api_key, @api_secret
         faraday.request  :url_encoded             # form-encode POST params
         #faraday.response :logger                  # log requests to STDOUT TODO set a client log file
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
